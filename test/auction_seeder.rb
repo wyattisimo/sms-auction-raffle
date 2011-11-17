5ad036a4f1d5ea2aba315df69b1569959352edd2
Jared Wyatt <j@wyatt.co>
2011-11-17 18:02:57 -0500

    read data in seeders

--- test/auction_seeder.rb
+++ test/auction_seeder.rb
@@ -10,11 +10,104 @@ db = Mongo::Connection.new.db(db_name)
 
 db[items_coll].remove
 
-(1..15).each do |i|
-  db[items_coll].insert({
-    'number' => i,
-    'name' => "Item #{i}",
-    'info' => "This is the info for auction item #{i}.",
-    'bids' => Array.new
-  })
-end
+# (1..15).each do |i|
+#   db[items_coll].insert({
+#     'number' => i,
+#     'name' => "Item #{i}",
+#     'info' => "This is the info for auction item #{i}.",
+#     'bids' => Array.new
+#   })
+# end
+
+# real data
+
+db[items_coll].insert({
+  'number' => 1,
+  'name' => "Startup Starter",
+  'info' => "Desk space, branding package, snacks, GA classes",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 2,
+  'name' => "Jetsetter",
+  'info' => "750 jetsetter credit and trip advisor",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 3,
+  'name' => "Staycation",
+  'info' => "1 night at Gansevoort, Gilt City Credit, Uber transport",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 4,
+  'name' => "Aha.life",
+  'info' => "Fun goodies from Aha.life",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 5,
+  'name' => "Custom Avatar",
+  'info' => "Designed by Alexis Moniello",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 6,
+  'name' => "Jack Robie",
+  'info' => "4 jack robie custom shirts",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 7,
+  'name' => "Loom Decor",
+  'info' => "250 Loom credit and inter decorator consult",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 8,
+  'name' => "Broodr",
+  'info' => "Geeky soap, wand remote, yoda doormat",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 9,
+  'name' => "Fitted Fashion",
+  'info' => "Custom tailoring for suit and shirts",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 10,
+  'name' => "Join Bklyn",
+  'info' => "Private tour of BK museum, signed print",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 11,
+  'name' => "Newscred",
+  'info' => "Newsfeed for your site, content consultation",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 12,
+  'name' => "CXXVI",
+  'info' => "Shirt curated by Svpply",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 13,
+  'name' => "Wintercheck",
+  'info' => "Scarf, Shot glass, wallet, curated by Svpply",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 14,
+  'name' => "DLC",
+  'info' => "Chain bracelet, curated by Svpply",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 15,
+  'name' => "graham winters",
+  'info' => "2 neck ties, curated by Svpply",
+  'bids' => Array.new
+})
---
+++ test/raffle_seeder.rb
@@ -0,0 +1,82 @@
+require 'rubygems'
+require 'mongo'
+
+@db_name = 'rc_raffle'
+@items_coll = 'prizes'
+@bidders_coll = 'hopefuls'
+
+db = Mongo::Connection.new.db(db_name)
+
+db[items_coll].remove
+
+# (1..15).each do |i|
+#   db[items_coll].insert({
+#     'number' => i,
+#     'name' => "Item #{i}",
+#     'info' => "This is the info for auction item #{i}.",
+#     'bids' => Array.new
+#   })
+# end
+
+# real data
+
+db[items_coll].insert({
+  'number' => 1,
+  'name' => "Starter Startup",
+  'info' => "Office space, programming, snacks, GA classes",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 2,
+  'name' => "NY Girl Style",
+  'info' => "Rent the Runway Credit, Warby Parker glasses",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 3,
+  'name' => "NY Guy Style",
+  'info' => "Bonobos credit, Warby Parker glasses",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 4,
+  'name' => "Chloe + Isabel",
+  'info' => "Jewelry Set",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 5,
+  'name' => "Broodr",
+  'info' => "Geeky soap, Yoda Doormat, Wand Remote",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 6,
+  'name' => "Small Girls",
+  'info' => "Clutch, necktie, glasses, refashioner credit",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 7,
+  'name' => "Fun Weekend",
+  'info' => "Movie passes, X box kinect, x box live membership",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 8,
+  'name' => "Other Fun Weekend",
+  'info' => "Movie passes, Xbox kinect, Xbox live membership",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 9,
+  'name' => "Something Borrowed",
+  'info' => "$400 Bridesmaid dress rental",
+  'bids' => Array.new
+})
+db[items_coll].insert({
+  'number' => 10,
+  'name' => "Artsicle",
+  'info' => "6 month art rental",
+  'bids' => Array.new
+})
