05e3fe6f7c58eb6da4cac59d6abdfb0b80e4ae0b
Jared Wyatt <j@wyatt.co>
2011-11-17 22:40:32 -0500

    raffle winners test

--- test/raffle_winners.rb
+++ test/raffle_winners.rb
@@ -11,12 +11,22 @@ db = Mongo::Connection.new.db(db_name)
 
 db[winners_coll].remove
 
+puts "\nDRAWING RANDOM WINNERS FOR EACH RAFFLE ITEM...\n"
+
 # step through the prizes and choose random winners
 db[items_coll].find.sort('number').each do |prize|
   
-  puts "#{prize['number']}. #{prize['name']}: size #{prize['bids'].size}"
-  prize['bids'].each do |bid|
-    bidder = db[bidders_coll].find_one({ 'phone' => bid['bidder_phone'] })
-    puts "    #{bidder['name']} #{bidder['phone']} #{bid['bidder_phone']}"
-  end
+  puts "#{prize['number']}. #{prize['name']}: #{prize['bids'].size} tickets entered"
+  
+  # pick random ticket
+  r_index = rand(prize['bids'].size-1)
+  puts "r_index: #{r_index}"
+  winner = db[bidders_coll].find_one({ 'phone' => prize['bids'][r_index]['bidder_phone'] })
+  puts "   WINNER: #{winner['name']} (#{winnder['phone']})"
+  
+  # list all tickets
+  # prize['bids'].each do |bid|
+  #   bidder = db[bidders_coll].find_one({ 'phone' => bid['bidder_phone'] })
+  #   puts "    #{bidder['name']} #{bidder['phone']}"
+  # end
 end
