62a56690b43094e0b4bf9ac6823e337a08caa5c9
Jared Wyatt <j@wyatt.co>
2011-11-18 13:12:52 -0500

    stats testing

--- app.rb
+++ app.rb
@@ -23,15 +23,16 @@ get %r{/stats/?} do
   i = 0
   items.each do |item|
     @items[i] = item
-    b = 0
-    item['bids'].each do |bid|
-      bidder = auction.get_bidder(bid['bidder_phone'])
-      @items[i]['bids'][b]['bidder_name'] = "#{bidder['name']}"
-      b += 1
-    end
-    @items[i]['bids'].sort_by! { |b| b['amount'] }
-    @items[i]['bids'].reverse!
-    @items[i]['high_bid'] = @items[i]['bids'][0]['amount']
+    @items[i]['high_bid'] = 0
+    # b = 0
+    # item['bids'].each do |bid|
+    #   bidder = auction.get_bidder(bid['bidder_phone'])
+    #   @items[i]['bids'][b]['bidder_name'] = "#{bidder['name']}"
+    #   b += 1
+    # end
+    # @items[i]['bids'].sort_by! { |b| b['amount'] }
+    # @items[i]['bids'].reverse!
+    # @items[i]['high_bid'] = @items[i]['bids'][0]['amount']
     i += 1
   end
   
