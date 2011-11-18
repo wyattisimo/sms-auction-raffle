4d0e5ccf622031a7d79bb02ff78fb628efc3e2bf
Jared Wyatt <j@wyatt.co>
2011-11-17 22:51:12 -0500

    raffle winners test

--- test/raffle_winners.rb
+++ test/raffle_winners.rb
@@ -20,7 +20,7 @@ db[items_coll].find.sort('number').each do |prize|
   
   # pick random ticket
   if prize['bids'].size > 0 then
-    r_index = rand(prize['bids'].size-1)
+    r_index = rand(prize['bids'].size)
     puts "r_index: #{r_index}"
     puts "r_index: #{r_index.round}"
     winner = db[bidders_coll].find_one({ 'phone' => prize['bids'][r_index]['bidder_phone'] })
