678e8391e098ab3ce550bcb7c3c60cf0565205e9
Jared Wyatt <j@wyatt.co>
2011-11-18 22:52:53 -0500

    test auction winners msg

--- auction_winners.rb
+++ auction_winners.rb
@@ -23,24 +23,28 @@ db[items_coll].find.sort('number').each do |item|
   
   if item['bids'] && item['bids'].size > 0 then
     item['bids'].sort_by! { |b| b['amount'].to_i }
-    high = item['bids'].last
+    
+    @client = Twilio::REST::Client.new $account_sid, $auth_token
+    
+    # notify winner ##############
+    
+    high = item['bids'].pop
+    
     winner = db[bidders_coll].find_one({ 'phone' => high['bidder_phone'] })
     
     puts "   HIGH BID: $#{high['amount']}"
     puts "   WINNER:   #{winner['name']} (#{winner['phone']})"
     
-    # notify winner
     puts "   ...sending SMS to the winner..."
-    win_msg = sprintf("Yay! You won the RaiseCache auction for \"#{item['name']}\"! Pls make your donation of $%d at http://bit.ly/tWjW4K. We'll contact you to arrange delivery.", high['amount'])
+    win_msg = sprintf("Yay! You won the RaiseCache auction for \"#{item['name']}\"! Pls make your donation of $%d at http://bit.ly/c4hackny. We'll contact you to arrange delivery.", high['amount'])
     puts "   #{win_msg}"
-    # @client = Twilio::REST::Client.new $account_sid, $auth_token
     # @client.account.sms.messages.create(
     #   :from => $auction_number,
-    #   :to => winner['phone'],
-    #   :body => msg
+    #   :to => '+18582480841',#winner['phone'],
+    #   :body => win_msg
     # )
-    
-    # save for posterity
+  
+    # save winner for posterity
     db[winners_coll].insert({
       'ts' => Time.now.to_s,
       'item_number' => item['number'],
@@ -50,6 +54,21 @@ db[items_coll].find.sort('number').each do |item|
       'winner_name' => winner['name'],
       'winner_phone' => winner['phone']
     })
+    
+    # notify losers ##############
+    
+    puts "   ...sending SMS to the losers..."
+    lose_msg = "Thanks for participating in the RaiseCache auction for \"#{item['name']}\"! Yours was not the highest bid, but RaiseCache for hackNY was a huge success!"
+    
+    item['bids'].each do |bid|
+      puts "   (#{bid['bidder_phone']}) #{lose_msg}"
+      # @client.account.sms.messages.create(
+      #   :from => $auction_number,
+      #   :to => '+18582480841',#winner['phone'],
+      #   :body => lose_msg
+      # )
+    end
+    
   end
   
 end
